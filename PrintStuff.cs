c13f336ab839e01db9ad3f7168f6be0ecb642c5a
asante <asante@ASANTELARYE228E>
2024-01-15 19:05:15 -0700

    Add project files.

---
+++ PrintStuff.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class PrintStuff
+    {
+
+        private string language;
+
+        //Constructor
+
+        public PrintStuff()
+        {
+            //Nothing passed in
+            language = "ENG";
+        }
+        public PrintStuff(string temp)
+        {
+            language = temp;
+        }
+        public void PrintName(string name)
+        {
+            if (language == "ENG")
+            {
+                System.Console.WriteLine("Hello, " + name, "!");
+            }
+            
+
+            if (language == "PT")
+            {
+                System.Console.WriteLine("Oi, " + name, "!");
+            }
+        }
+    }
+}
